30bf42b071419bd910a4ac222865f43c3b3a6898
Sarabjit Singh <websachin111@gmail.com>
2022-10-20 19:18:09 +0530

    change title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Sarabjit Singh";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Next.js Blog Website";
 
 export default function Layout({ children, home }) {
   return (
